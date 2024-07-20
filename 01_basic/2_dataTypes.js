83998b8f9df1dabc42b43bdb62d60c09d3d9f763
dSAxmonis <154427061+dSAxmonis@users.noreply.github.com>
2024-07-20 08:25:44 +0000

    datatypes done

---
+++ 01_basic/2_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+// alert(3+2) //we are using nodejs, not browser
+
+console.log(3 + 2) 
+
+let name = "hitesh"
+let age = 18
+let isLogIn = false
+
+// number => 2 to power 42
+// bogint
+// boolean => true/false
+// null => standalone value
+// symbole => unique 
+// object
+
+console.log(typeof null); // gives Object
+
