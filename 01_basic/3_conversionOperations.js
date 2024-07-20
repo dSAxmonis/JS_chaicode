f92439fd1cda792e149e1bcf49ea427f7d19eb10
dSAxmonis <154427061+dSAxmonis@users.noreply.github.com>
2024-07-20 09:55:44 +0000

    dataType conversion

---
+++ 01_basic/3_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "alien"
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber) 
+
+//"33"=> 33
+// "33abc" => NaN but type is converted to number 
+// true => 1; false => 0 viceversa
+// "" => false
+// "kuch bhi" => true
+
+
+// ******************************Operations********************
+
+let  value = 3
+let negValue = -value
+// console.log(negValue);
+
+console.log(2*3);
+console.log(2**3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2;
+console.log(str3)
+
+
+
+
